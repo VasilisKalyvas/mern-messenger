f03df3e526cbd5ac75780c31f400f972b626a50e
Βασιλης Καλυβας <bkalivas8@gmail.com>
2022-05-28 19:22:17 +0300

    make it better

--- src/Components/SingleChat.js
+++ src/Components/SingleChat.js
@@ -105,7 +105,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
   useEffect(() => {
     socket = io(ENDPOINT);
-    socket.emit("setup", () => setSocketConnected(false), user);
+    socket.emit("setup", () => {setSocketConnected(false), user});
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
     socket.on("stop typing", () => setIsTyping(false));
